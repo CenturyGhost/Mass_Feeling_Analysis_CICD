0680633b5edca400cba036408ab13e5b0c7a2578
Alexandre NOUAR <49387527+CenturyGhost@users.noreply.github.com>
2022-08-21 09:59:10 +0200

    Update jenkins.features.groovy

--- jenkins.features.groovy
+++ jenkins.features.groovy
@@ -1,1 +1,21 @@
+def build_app(){
+  sh 'docker build -t tender_matsumoto .'
+}
 
+def run_app(){
+  sh 'docker run -p 5000:5000 tender_matsumoto'
+}
+
+def release_app(){
+  echo 'TALEX IS ON FIRE'
+}
+
+def accept_app(){
+  input 'Proceed to live development ?'
+}
+
+def merge_app(){
+  echo 'merging to master'
+}
+
+return this
